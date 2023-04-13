cfb2518c7ec4730a62a969d98fba55650f3f8219
Manya-singh2001 <manyasinghsingh16699@gmail.com>
2023-04-13 15:43:54 +0530

    addition of assets

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {MenuIcon, XIcon} from '@heroicons/react/outline'
+
+const Navbar = () => {
+  return (
+    <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
+        <div>
+            
+        </div>
+      
+<MenuIcon className='w-8' />
+    </div>
+  )
+}
+
+export default Navbar
